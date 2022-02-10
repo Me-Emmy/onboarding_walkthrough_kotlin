75ecb2da50b20db08665d86ce3ef8f42cf2a9e2a
Emmy <99374791+Me-Emmy@users.noreply.github.com>
2022-02-10 07:52:24 +0530

    Update SecondScreen.kt

--- SecondScreen.kt
+++ SecondScreen.kt
@@ -1,15 +1,13 @@
-package com.emmywallpapers.onboarding.screens
+package com.onboarding
 
 
-import com.emmywallpapers.databinding.FragmentSecondScreenBinding
+import com.onboarding.databinding.FragmentSecondScreenBinding
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.viewpager2.widget.ViewPager2
-import com.emmywallpapers.R
-
 
 
 class SecondScreen : Fragment() {
@@ -20,7 +18,6 @@ class SecondScreen : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding= FragmentSecondScreenBinding.inflate(inflater,container,false)
 
         val viewPager =  activity?.findViewById<ViewPager2>(R.id.viewPager)
